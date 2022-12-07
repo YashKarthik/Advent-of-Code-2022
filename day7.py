fc0dff40d707488f5f514abda716b1c0d87bb65e
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2022-12-07 11:47:55 +0530

    Day 7

---
+++ day7.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+lines = map(str.split, open('./input7.txt').read().splitlines())
+path, dirs = [], defaultdict(int)
+
+for l in lines:
+    if l[0] == "$":
+        if l[1] == "cd":
+            if l[2] == "..":
+                path.pop()
+            else:
+                path.append(l[2])
+    elif l[0] != "dir":
+        for i in range(len(path)):
+            dirs[tuple(path[: i + 1])] += int(l[0])
+
+print(sum(size for size in dirs.values() if size <= 100000))
+
+required = 30000000 - (70000000 - dirs[("/",)])
+
+print(min(size for size in dirs.values() if size >= required))
